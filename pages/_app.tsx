bd452b62f58f0180dd20777523209ded9a55b4b7
mingming <jimin23623@gmail.com>
2024-04-13 13:53:11 +0900

    chore: lint import 순서, 그룹화 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
-import type { AppProps } from 'next/app';
+import { HydrationBoundary } from '@tanstack/react-query';
 import { ThemeProvider } from 'styled-components';
-import theme from '../src/app/styles/theme/theme';
+
 import GlobalStyle from '../src/app/styles/GlobalStyle';
+import theme from '../src/app/styles/theme/theme';
+
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <HydrationBoundary state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </HydrationBoundary>
     </ThemeProvider>
   );
 }
