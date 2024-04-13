98f804b2ff131d00081cf83b45f705807636d2ad
mingming <jimin23623@gmail.com>
2024-04-13 12:59:40 +0900

    chore: FSD architecture

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import GlobalStyle from '@/styles/GlobalStyle';
+import GlobalStyle from '@/app/styles/GlobalStyle';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
-import theme from '../styles/theme/theme';
+import theme from '../src/app/styles/theme/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
