146479fd291d627349626324196bab2f0da733af
mingming <jimin23623@gmail.com>
2024-04-13 13:13:39 +0900

    chore: 절대 경로 지정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import GlobalStyle from '@/app/styles/GlobalStyle';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import theme from '../src/app/styles/theme/theme';
+import GlobalStyle from '../src/app/styles/GlobalStyle';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
