2b897d093b117482a5771da5a94b55a0d77587e5
mingming <jimin23623@gmail.com>
2024-04-15 20:02:11 +0900

    dependency: storybook에 globalStyle 적용시키기

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,8 @@
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
 import type { Preview } from '@storybook/react';
+import GlobalStyle from '../src/shared/ui/styles/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../src/shared/ui/styles/theme/theme';
 
 const preview: Preview = {
   parameters: {
@@ -10,5 +14,13 @@ const preview: Preview = {
     },
   },
 };
+
+export const decorators = [
+  withThemeFromJSXProvider({
+    themes: theme,
+    Provider: ThemeProvider,
+    GlobalStyles: GlobalStyle,
+  }),
+];
 
 export default preview;
